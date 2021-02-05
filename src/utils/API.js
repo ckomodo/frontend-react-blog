78b8c4a404ee3f757ba6cfbfe9a3aa994a0a8636
ckomodo <chrissakwa@gmail.com>
2021-02-04 19:28:12 -0800

    pushing to heroku

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
-const URL_PREFIX = "http://localhost:8080"
-// const URL_PREFIX = "https://chrisreactblog.herokuapp.com"
+// const URL_PREFIX = "http://localhost:8080"
+const URL_PREFIX = "https://chrisreactblog.herokuapp.com"
 
 
 
